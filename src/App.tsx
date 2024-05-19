50c4391922b126632cfd3530ca69191c33984a1b
Afagh <afagh@Afaghs-MacBook-Pro.local>
2024-05-19 15:44:56 +0200

    Start Project
    Setting the structure of the code.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+
+interface Todo  {
+    id: number,
+    task: string,
+    isCompleted: boolean,
+    createdAt: string,
+    order: number,
+}
+
+
+function App() {
+      return (
+          <div>
+            <h1>This is just the start!</h1>
+          </div>
+      );
+}
+
+export default App;
